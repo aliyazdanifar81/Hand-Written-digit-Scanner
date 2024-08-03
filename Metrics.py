5c70a83a470329f77d927b5cf11e2ced13a94a4b
ALI YAZDANIFAR <yazdanifar1381@gmail.com>
2024-08-03 10:44:52 +0330

    update

--- Metrics.py
+++ Metrics.py
@@ -2,7 +2,7 @@ import Levenshtein
 
 
 def cer(true: list, pred: list):
-    true, true = tuple(true), tuple(true)
+    true, pred = tuple(true), tuple(pred)
     distance = Levenshtein.distance(true, pred)
     cer = (distance / len(true)) * 100
     return cer
